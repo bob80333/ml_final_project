b4ccc98d1d133b4e4b69f0fb7620218fc7ab583d
Eric Engelhart <bob80333@gmail.com>
2023-11-26 22:10:53 -0500

    Create checkpoint dir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import trainer
 import tester
 import argparse
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -8,6 +9,11 @@ if __name__ == "__main__":
     parser.add_argument("--train", help="train the model", action="store_true")
     parser.add_argument("--test", help="test the model", action="store_true")
     args = parser.parse_args()
+    
+    if args.train or args.test:
+        # make sure checkpoint dir exists
+        if not os.path.exists("checkpoints"):
+            os.mkdir("checkpoints")
 
     if args.train:
         print("Training the model")
